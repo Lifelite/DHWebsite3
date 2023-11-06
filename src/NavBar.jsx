e47b149db74aea9302ab13ec7b850db75e59241a
Jeremy Huntsman <me@drunkenhuntsman.com>
2023-11-05 23:11:18 -0500

    Still fixing planetscale

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -64,6 +64,7 @@ export default function NavBar() {
             "stuff",
             process.env.PLANETSCALE_DB_HOST,
             import.meta.env.VITE_PLANETSCALE_DB,
+            process.env.VITE_PS_DATABASE
             )
     };
 
