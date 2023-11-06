f3f59049b098067b394ce9aeb4516fbc4c293432
Jeremy Huntsman <me@drunkenhuntsman.com>
2023-11-05 20:41:16 -0500

    Fixed an issue with MySQL

--- src/mySQL.js
+++ src/mySQL.js
@@ -3,12 +3,13 @@ import {connect} from "@planetscale/database";
 
 
 const config = {
-    url: process.env.DATABASE_URL,
+    //url: process.env.DATABASE_URL,
     host: process.env.PS_HOST,
     username: process.env.PS_USERNAME,
     password: process.env.PS_PASSWORD,
     database: process.env.PS_DATABASE
 }
+
 
 const conn = await connect(config)
 
@@ -47,7 +48,7 @@ export class SantaSubmit {
             nsfw: this.nsfw,
         };
 
-        const query = 'INSERT INTO SecretSanta ("firstName", "lastName", "email", "discord", "address1", "address2", "state", "zip", "likes", "dislikes", "charity", "allergies", "nsfw") VALUES (:firstName :lastName :email :discord :address1 :address2 :state :zip :likes :dislikes :charity :allergies :nsfw)'
+        const query = "INSERT INTO SecretSanta (`firstName`, `lastName`, `email`, `discord`, `address1`, `address2`, `state`, `zip`, `likes`, `dislikes`, `charity`, `allergies`, `nsfw`) VALUES (:firstName, :lastName, :email, :discord, :address1, :address2, :state, :zip, :likes, :dislikes, :charity, :allergies, :nsfw);";
 
         return await conn.execute(query, params);
     };
