c353341f42f9469f5e8bbaaa6a9725b7ac585709
Jeremy Huntsman <me@drunkenhuntsman.com>
2023-11-06 12:53:28 -0500

    fixing server functions

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,24 @@
 // import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import {defineConfig} from "vite";
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     target: 'esnext'
-//   }
-// });
-
-module.exports = {
-  env: {
-    PS_HOST: import.meta.env.env.PS_HOST,
-    PS_USERNAME: import.meta.env.env.PS_USERNAME,
-    PS_PASSWORD: import.meta.env.env.PS_PASSWORD,
-    PS_DATABASE: import.meta.env.env.PS_DATABASE,
-  },
+export default defineConfig({
   plugins: [react()],
   build: {
     target: 'esnext'
   }
-}
+});
+//
+// module.exports = {
+//   env: {
+//     PS_HOST: import.meta.env.env.PS_HOST,
+//     PS_USERNAME: import.meta.env.env.PS_USERNAME,
+//     PS_PASSWORD: import.meta.env.env.PS_PASSWORD,
+//     PS_DATABASE: import.meta.env.env.PS_DATABASE,
+//   },
+//   plugins: [react()],
+//   build: {
+//     target: 'esnext'
+//   }
+// }
